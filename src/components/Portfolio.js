5c032cf667fc9ece97922c1be76f91f61ee69612
Spencer <spencer@spencerteel.com>
2022-09-22 15:12:42 -0500

    Update Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -122,7 +122,7 @@ const Portfolio = () => {
                   >
                     <a
                       className="popup-youtube"
-                      href="https://www.youtube.com/watch?v=5dsN3hxjBrg?autoplay=1"
+                      href="https://www.youtube.com/embed/5dsN3hxjBrg?autoplay=1"
                     >
                       <img src="img/thumbs/42-56.jpg" alt="image" />
                       <div
